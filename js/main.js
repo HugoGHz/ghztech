940c23604d7b001764f98811580fbb9260eb4c15
Hugo GHz <107895251+HugoGHz@users.noreply.github.com>
2022-07-11 19:17:08 -0500

    Added responsive CSS file

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ const menu = document.getElementById('cards')
 button.addEventListener('click', ()=>{
     menu.classList.toggle('active')
 })
+
+const btnmenu = document.getElementById('btnicon')
+const bannermenu = document.getElementById('principal')
+const bannersearch = document.getElementById('search')
+
+btnmenu.addEventListener('click', ()=>{
+    bannermenu.classList.toggle('active')
+    bannersearch.classList.toggle('active')
+})
